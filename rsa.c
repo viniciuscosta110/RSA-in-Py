0f2e51131387fa0953b12823941a0ddcf06a1dc6
Leonardo Zaia <57048196+leozaia-h@users.noreply.github.com>
2020-01-16 00:37:49 -0300

    Update rsa.c

--- rsa.c
+++ rsa.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 
+int MDC(int p, int x)
+{
+    if(x == 0)
+        return p;
+    else
+        return MDC(x, p % x);
+}
+
 int main(void)
 {
     int p = 101701;
     int q = 101719;
     int n = p * q;
-
-    int totiente = 
+    int totiente_n = ((p - 1) * (q - 1));
+    
+    
 
     return 0;
 }
