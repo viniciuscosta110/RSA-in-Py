ea9deee98660f123efee7f44e2396f9afea49d04
Leonardo Zaia <57048196+leozaia-h@users.noreply.github.com>
2020-01-28 01:03:49 -0300

    Update rsa.py

--- rsa.py
+++ rsa.py
@@ -35,67 +35,6 @@ def MDC(p, q):
 def main():
     print("AQUI VAI AS ESCOLHAS")
 
-    escolha = 1
-
-    if(escolha == 1):
-        print("Deseja escolher os numeros primos?")
-        print("1: SIM                       2:NÃO")
-        escolha_primos = 1
-
-        if(escolha_primos == 1):
-            p = 3
-            q = 11
-            texto = 9
-
-            n = p * q
-
-        totiente = qntd_totiente(p, q)
-
-        e = primos_entre_si(n)
-        d = achar_d(e, totiente)
-
-        print(criptografar(texto, e, n))
-        print("cahve publica: {}".format(e))
-        print("chave privada: {}".format(d))
-main()
-
-import random
-
-def criptografar(texto, e, n):
-    encriptado = (texto ** e) % n
-    return encriptado
-
-def achar_d(e, totiente):
-    i = 1
-    while(True):
-        if(e * i % totiente == 1):
-            return i
-        i += 1
-
-def primos_entre_si(n):
-    resultado = []
-
-    for i in range(n):
-        if(MDC(n, i) == 1):
-            resultado.append(i)
-    return random.choice(resultado)
-
-def qntd_totiente(p, q):
-    return (p - 1) * (q - 1)
-    
-def MDC(p, q):
-    if(p < q):
-        aux = p
-        p = q
-        q = aux
-    if(q == 0):
-        return p
-    else:
-        return MDC(q, p%q)
-
-def main():
-    print("AQUI VAI AS ESCOLHAS")
-
     escolha = int(input())
 
     if(escolha == 1):
