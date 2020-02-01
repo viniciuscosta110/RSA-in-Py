2be324beb7e59c96b5779183215c5e6c6dbda4db
Leonardo Zaia <57048196+leozaia-h@users.noreply.github.com>
2020-02-01 14:14:38 -0300

    Update rsa.py

--- rsa.py
+++ rsa.py
@@ -1,12 +1,16 @@
+import os
+import math
+
 def limpar_terminal():
-    for i in range(40):
-        print()
+    if (os.name == "nt"):
+        os.system("cls")
+    else:
+        os.system("clear")
 
 def verificar_primos(x):
     if(x <= 1):
         return False
-
-    for c in range(2,x):
+    for c in range(2,math.sqrt(x) + 1):
         if(x % c == 0):
             return False
     return True
@@ -73,102 +77,124 @@ def ler_msg_descriptografada():
     arquivo.close
     return x
 
-def main():
-    limpar_terminal()
-    print("  <==================================================>")
-    print("  <=        ENCRIPTAÇÃO E DESENCRIPTAÇÃO RSA        =>")
-    print("  <==================================================>")
-    print("  <=        Escolha uma das opções á seguir:        =>")
-    print("  <=                                                =>")
-    print("  <= 1: GERAR CHAVE PUBLICA                         =>")
-    print("  <= 2: ENCRIPTAR                                   =>")
-    print("  <= 3: DESENCRIPTAR                                =>")
-    print("  <= 4: SAIR                                        =>")
-    print("  <==================================================>")
-    escolha = int(input("     >Digite sua escolha: " ))
-    limpar_terminal()
+def criptografar(c,e,n):
+    array = ['@','@','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',' ']
+    aux = 0
+    for i in range(0,29):
+        if(c == array[i]):
+            aux = i
+    M = pow(aux, e)
+    M = M % n
+    return M
 
-    if(escolha == 1):
+def desencriptar(c, d, n):
+    array = ['@','@','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',' ']
+
+def main():
+    while(1):
         print("  <==================================================>")
-        print("  <=              Gerando chave pública             =>")
+        print("  <=        ENCRIPTAÇÃO E DESENCRIPTAÇÃO RSA        =>")
         print("  <==================================================>")
-        print("  <= Digite os números primos:                      =>")
+        print("  <=        Escolha uma das opções á seguir:        =>")
+        print("  <=                                                =>")
+        print("  <= 1: GERAR CHAVE PUBLICA                         =>")
+        print("  <= 2: ENCRIPTAR                                   =>")
+        print("  <= 3: DESENCRIPTAR                                =>")
+        print("  <= 4: SAIR                                        =>")
         print("  <==================================================>")
-        p = int(input("     (p):"))
-        q = int(input("     (q):"))
-        if(p * q < 28):
-            while(p * q < 28):
-                print("  <==================================================>")
-                print("  <=          (p) e (q) são muito pequenos!         =>")
-                print("  <=              Digite-os novamente!              =>")
-                print("  <==================================================>")
-                p = int(input("      Digite (p):"))
-                q = int(input("      Digite (q):"))
-        if(verificar_primos(p) == False or verificar_primos(q) == False):
-                while(verificar_primos(p) == False or verificar_primos(q) == False):
-                    limpar_terminal()
+        escolha = int(input("    >Digite sua escolha: " ))
+        limpar_terminal()
+    
+        if(escolha == 1):
+            print("  <==================================================>")
+            print("  <=              GERANDO CHAVE PÚBLICA             =>")
+            print("  <==================================================>")
+            print("  <= Digite os números primos:                      =>")
+            print("  <==================================================>")
+            p = int(input("     (p):"))
+            q = int(input("     (q):"))
+            if(p * q < 28):
+                while(p * q < 28):
                     print("  <==================================================>")
-                    print("  <=  Um ou mais números digitados não são primos!  =>")
+                    print("  <=          (p) e (q) são muito pequenos!         =>")
                     print("  <=              Digite-os novamente!              =>")
                     print("  <==================================================>")
                     p = int(input("      Digite (p):"))
                     q = int(input("      Digite (q):"))
-        n = p * q
-        phi = calcular_phi(p, q)
-        limpar_terminal()
-        print("  <==================================================>")
-        print("  <=    INSIRA O (e) PARA GERAR A CHAVE PÚBLICA     =>")
-        print("  <==================================================>")
-        e = int(input("     (e): "))
-        while(primos_entre_si(e,phi) == 0 or e >= phi or e <= 1):
+            if(verificar_primos(p) == False or verificar_primos(q) == False):
+                    while(verificar_primos(p) == False or verificar_primos(q) == False):
+                        limpar_terminal()
+                        print("  <==================================================>")
+                        print("  <=  Um ou mais números digitados não são primos!  =>")
+                        print("  <=              Digite-os novamente!              =>")
+                        print("  <==================================================>")
+                        p = int(input("      Digite (p):"))
+                        q = int(input("      Digite (q):"))
+            n = p * q
+            phi = calcular_phi(p, q)
             limpar_terminal()
-            if(primos_entre_si(e, phi) == 0 and e < phi and e > 1):
-                print(" =====================================================")
-                print(" ==  O número digitado não é coprimo ao phi!        ==")
-                print(" =====================================================")
-                print("  ==  Digite novamente:                             ==")
-            elif(e >= phi):
-                print(" =====================================================")
-                print(" ==  O número digitado é maior ou igual a phi!      ==")
-                print(" =====================================================")
-                print("  ==  Digite novamente:                             ==")
-            elif(e <= 1):
-                print(" =====================================================")
-                print(" ==  O número digitado é menor ou igual a 1         ==")
-                print(" =====================================================")
-                print("  ==  Digite novamente:                             ==")
-
+            print("  <==================================================>")
+            print("  <=    INSIRA O (e) PARA GERAR A CHAVE PÚBLICA     =>")
+            print("  <==================================================>")
             e = int(input("     (e): "))
-        print("    Chave públuca: {} {}".format(n, e))
-    elif(escolha == 2):
-        limpar_terminal()
-        print("  <==================================================>")
-        print("  <===== DIGITE A SUA MENSAGEM PARA ENCRIPTA-LA =====>")
-        print("  <==================================================>")
-        texto = input("  == > ")
-        limpar_terminal()
-        print("  <==================================================>")
-        print("  <=          PRECISAMOS DA CHAVE PUBLICA!          =>")
-        print("  <=              Digite o (n) e o (e)              =>")
-        print("  <==================================================>")
-        n = int(input("  == (n): "))
-        e = int(input("  == (e): "))
-        texto_encriptado = encriptar()
-
-    elif(escolha == 3):
-        limpar_terminal()
-        print("  <==================================================>")
-        print("  <=     PARA DESENCRIPTAR INSIRA OS VALORES DE:    =>")
-        print("  <=                 (p), (q) e (e)                 =>")
-        print("  <==================================================>")
-        p = int(input("    (p): "))
-        q = int(input("    (q): "))
-        e = int(input("    (e): "))
-
-    elif(escolha == 4):
-        exit()
-    else:
-        print("Escolha inválida!")
-
-        
+            while(primos_entre_si(e,phi) == 0 or e >= phi or e <= 1):
+                limpar_terminal()
+                if(primos_entre_si(e, phi) == 0 and e < phi and e > 1):
+                    print("  <==================================================>")
+                    print("  <=     O NÚMERO DIGITADO NÃO É COPRIMO A PHI!     =>")
+                    print("  <=              Digite (e) novamente:             =>")
+                    print("  <==================================================>")
+                elif(e >= phi):
+                    print("  <==================================================>")
+                    print("  <=    O NÚMERO DIGITADO É MAIOR OU IGUAL A PHI!   =>")
+                    print("  <=              Digite (e) novamente:             =>")
+                    print("  <==================================================>")
+                elif(e <= 1):
+                    print("  <==================================================>")
+                    print("  <=     O NÚMERO DIGITADO É MENOR OU IGUAL A 1!    =>")
+                    print("  <=              Digite (e) novamente:             =>")
+                    print("  <==================================================>")
+    
+                e = int(input("     (e): "))
+            limpar_terminal()
+            print("  ####################################################")
+            print("              CHAVE PÚBLICA: (n):{} (e):{}            ".format(n, e))
+            print("  ####################################################")
+        elif(escolha == 2):
+            limpar_terminal()
+            print("  <==================================================>")
+            print("  <=          PRECISAMOS DA CHAVE PUBLICA!          =>")
+            print("  <=              Digite o (n) e o (e)              =>")
+            print("  <==================================================>")
+            n = int(input("  == (n): "))
+            e = int(input("  == (e): "))
+            print("  <==================================================>")
+            print("  <=     DIGITE A SUA MENSAGEM PARA ENCRIPTA-LA     =>")
+            print("  <==================================================>")
+            texto = input("    > ")
+            texto = texto.upper()
+            limpar_terminal()
+            for i in range(len(texto)):
+                print(" ", criptografar(texto[i], e, n), end="")
+            print()
+    
+        elif(escolha == 3):
+            limpar_terminal()
+            print("  <==================================================>")
+            print("  <=   DIGITE A SUA MENSAGEM PARA DESENCRIPTA-LA!   =>")
+            print("  <==================================================>")
+            texto = input("    > ")
+            print("  <==================================================>")
+            print("  <=     PARA DESENCRIPTAR INSIRA OS VALORES DE:    =>")
+            print("  <=                    (n) e (d)                   =>")
+            print("  <==================================================>")
+            n = int(input("    (n): "))
+            d = int(input("    (d): "))
+    
+        elif(escolha == 4):
+            exit()
+        else:
+            print("  <!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!>")
+            print("  <!!               Escolha inválida               !!>")
+            print("  <!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!>")
 main()
